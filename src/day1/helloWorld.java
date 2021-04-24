10a45f325e2af12995100f6650c1d755a1127091
Deum <oscar.sdet@gmail.com>
2021-04-24 18:17:37 -0400

    said hello oscar

--- src/day1/helloWorld.java
+++ src/day1/helloWorld.java
@@ -4,5 +4,7 @@ public class helloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Adding githuh now ");
+        System.out.println("hello oscar");
+        //hello
     }
 }
